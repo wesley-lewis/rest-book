f42e2ccd9a7ea243655e950d47b64cd292091262
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-23 01:12:15 +0530

    hellohandler

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	app := fiber.New()
+	app.Get("/", helloWorldHandler)
 	fmt.Println("rest book")
+	if err := app.Listen(":8000"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloWorldHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello world\n")
 }
