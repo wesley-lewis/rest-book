e9e941c0f3bf3f03b0a1fb863d2f542fbc401a9e
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-23 22:22:34 +0530

    memorystore added for testing

---
+++ storage/memorystore.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"rest-book/model"
+	"fmt"
+	"strconv"
+)
+
+type MemoryStore struct {
+	Restaurants []*model.Restaurant
+}
+
+func NewMemoryStore() Store{
+	return &MemoryStore{
+	}
+}
+func (m *MemoryStore) GetRestaurantDetails(id string) (*model.Restaurant, error) {
+	idx, err := strconv.Atoi(id)
+	if idx > len(m.Restaurants) - 1 {
+		return nil, fmt.Errorf("no restaurant")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return  m.Restaurants[idx], nil
+}
+
+func(m *MemoryStore) AddRestaurantDetails(rest *model.Restaurant) error {
+	m.Restaurants = append(m.Restaurants, rest)
+	return nil
+}
