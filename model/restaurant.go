17098fa1aff3436736f08a34c1793315fa2426d3
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-23 22:21:46 +0530

    string representation of restaurant

--- model/restaurant.go
+++ model/restaurant.go
@@ -1,7 +1,14 @@
 package model 
 
+import (
+	"fmt"
+)
 type Restaurant struct {
 	Name		string		`json:"name_of_restaurant"`
 	Address		*Address	`json:"address"`	
-Timings		*Timing		`json:"timing"`
+	Timings		*Timing		`json:"timing"`
+}
+
+func( r *Restaurant) String() string {
+	return fmt.Sprintf("Name: %s | Address: %s %s %s", r.Name, r.Address.Building, r.Address.Street, r.Address.Landmark)
 }
