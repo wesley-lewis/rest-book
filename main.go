4e366ae3b99246e143c88d0fc9254a8b101bf16d
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-24 00:18:44 +0530

    added email and id

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
+import "github.com/joho/godotenv"
+
 
 func main() {
+	godotenv.Load()
 	s := NewServer(":8000")
 	
 	s.Start()
--- model/restaurant.go
+++ model/restaurant.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 type Restaurant struct {
+	Id			string		`json:"id"`
+	Email		string		`json:"email"`
 	Name		string		`json:"name_of_restaurant"`
 	Address		*Address	`json:"address"`	
 	Timings		*Timing		`json:"timing"`
