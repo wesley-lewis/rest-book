1a7802d3e6e1d2ef1976ef4ecc58a05f8ef82600
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-27 15:42:25 +0530

    separate model for adding and retrieval

--- model/restaurant.go
+++ model/restaurant.go
@@ -1,14 +1,23 @@
-package model 
+package model
 
 import (
 	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 type Restaurant struct {
-	Id			string		`bson:"_id" json:"id"`
-	Email		string		`json:"email"`
-	Name		string		`json:"name_of_restaurant"`
-	Address		*Address	`json:"address"`	
-	Timings		*Timing		`json:"timing"`
+	Email		string				`json:"email"`
+	Name		string				`json:"name_of_restaurant"`
+	Address		*Address			`json:"address"`	
+	Timings		*Timing				`json:"timing"`
+}
+
+type RestaurantDb struct {
+	Id			primitive.ObjectID	`bson:"_id" json:"id"`
+	Email		string				`json:"email"`
+	Name		string				`json:"name_of_restaurant"`
+	Address		*Address			`json:"address"`	
+	Timings		*Timing				`json:"timing"`
 }
 
 func( r *Restaurant) String() string {
