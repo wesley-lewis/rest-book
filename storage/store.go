c309ccaec3e1eff4586cb06bbbb49bd33b6a93e6
wesley-lewis <lewis.wes.23@gmail.com>
2023-12-31 11:42:07 +0530

    added api for users to storage

--- storage/store.go
+++ storage/store.go
@@ -1,12 +1,16 @@
-package storage 
+package storage
 
 import (
 	"rest-book/model"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 type Store interface {
-	GetRestaurantDetails(string)						(*model.Restaurant, error )
+	GetRestaurantDetails(string)					(*model.Restaurant, error )
 	AddRestaurantDetails(*model.Restaurant)				error
 	UpdateRestaurantDetails(string, *model.Restaurant)	(error)
 	GetAllRestaurantDetails()							([]*model.RestaurantDb, error)
-	DeleteRestaurantDetails(string)						(error) 
+	DeleteRestaurantDetails(string)				(error) 
+	AddUser(*model.User)									(primitive.ObjectID, error)
+	GetUsers()														([]*model.UserDb, error)
 }
