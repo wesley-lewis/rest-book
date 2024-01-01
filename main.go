8affa9a4acc541ec8313a442518f15c7a4947b5a
wesley-lewis <lewis.wes.23@gmail.com>
2024-01-01 18:22:58 +0530

    working push

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"rest-book/api"
+    "github.com/joho/godotenv"
+    "rest-book/api"
 )
 
 func main() {
-	godotenv.Load()
-	s := api.NewServer(":8000")
-	
-	s.Start()
+    godotenv.Load()
+    s := api.NewServer(":8000")
+
+    s.Start()
 }
